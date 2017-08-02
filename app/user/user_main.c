d25591e0cade91cc3c93561fa01fbe86764cdb57
zfj <zfj1441@gmail.com>
2017-08-02 18:05:19 +0800

    test

--- app/user/user_main.c
+++ app/user/user_main.c
@@ -1,248 +1,132 @@
-/******************************************************************************
- * Copyright 2013-2014 Espressif Systems (Wuxi)
- *
- * FileName: user_main.c
- *
- * Description: entry file of user application
- *
- * Modification history:
- *	 2014/1/1, v1.0 create this file.
-*******************************************************************************/
-
+#include "ets_sys.h"
+#include "os_type.h"
+#include "osapi.h"
 #include "mem.h"
+#include "user_interface.h"
+#include "espconn.h"
+#include "gpio.h"
 
-#include "ets_sys.h"  
-#include "gpio.h"   
-#include "osapi.h"   
-#include "os_type.h"	
-#include "user_interface.h"   
-#include "espconn.h"   
-#include "cJSON.h"
-	 
-#define ESPCONN_MAXMUTLI 5
-#define ESPCONN_MAXLENGTH 1460
 
-typedef struct _TransportInfo{
-	struct espconn *EspconnListen;
-	bool ListenFlag;
-}TransportInfo;
-
-TransportInfo TransportList[ESPCONN_MAXMUTLI];
-LOCAL os_timer_t EspconnMutliSendTimer;
-
-LOCAL os_timer_t timer;   
-LOCAL int zt = 0;   			// GPIO stat
 LOCAL int keystat = 0;
 
-void Espconn_MutliSend(char* szSendData)
-{
-	uint8 MutliSendCount = 0;
-	static uint32 MutliSendNum = 0;
-	char SendBufferOfMutli[ESPCONN_MAXLENGTH];
-//	os_memset(SendBufferOfMutli, MutliSendNum, ESPCONN_MAXLENGTH);
-//	os_memcpy(SendBufferOfMutli, "hello world", 11);
-	for (MutliSendCount = 0; MutliSendCount < ESPCONN_MAXMUTLI; MutliSendCount ++){
-		if (TransportList[MutliSendCount].ListenFlag){			
-			espconn_send(TransportList[MutliSendCount].EspconnListen, 
-				szSendData, os_strlen(szSendData));
-		}
-	}
-}
-
-void Espconn_MutliRemove(void *arg)
-{
-	TransportInfo *TransportInfoOfRemove = arg;
-	uint8 TransportListCount = 0;
-	for (TransportListCount = 0; TransportListCount < ESPCONN_MAXMUTLI; TransportListCount ++){
-		if (TransportInfoOfRemove == &TransportList[TransportListCount]){			
-		   TransportList[TransportListCount].ListenFlag = FALSE;
-		   TransportList[TransportListCount].EspconnListen = NULL;
-		   break;
-		}
-	}
-}
-
-void Espconn_MutliDisFn(void *arg)
-{
-	struct espconn *EspconnOfMutliDis = arg;
-	if (EspconnOfMutliDis != NULL)
-		Espconn_MutliRemove(EspconnOfMutliDis->reverse);
-}
-
-void Espconn_MutliRecvFn(void *arg, char *pdata, unsigned short len)
-{
-	int		iArraySize = 0, index = 0;
-	char	szRet[1460], tmp[10];
-	char	*out;
-	cJSON	*json, *root, *outJosn;
-	cJSON	*itemArray, *item;
-
-	os_memset(szRet, 0, sizeof(szRet));
-	struct espconn *EspconnOfMutliRecv = arg;
-	json=cJSON_Parse(pdata);
-	if (!json) {
-		os_sprintf(szRet, "Error before: [%s]\n",cJSON_GetErrorPtr());
-	}
-	else
-	{
-		do {
-			item=cJSON_GetObjectItem(json, "method");
-			if(!item) {
-				os_sprintf(szRet, "json data have not an method");
-				break ;
-			}
-			itemArray=cJSON_GetObjectItem(json, "params");
-			if(!itemArray || itemArray->type != cJSON_Array ) {
-				os_sprintf(szRet, "params is not an array");
-				break ;
-			}
-			if(os_strcmp(item->valuestring, "get_prop") == 0) {
-				if(GPIO_INPUT_GET(2)==1)
-					os_strcat(szRet, "{\"id\":-1,\"method\":\"get_prop\",\"power\":\"on\"}");
-				else
-					os_strcat(szRet, "{\"id\":-1,\"method\":\"get_prop\",\"power\":\"off\"}");
-			}
-			else if(os_strcmp(item->valuestring, "set_power") == 0) {
-				;
-			}
-			else if(os_strcmp(item->valuestring, "set_bright") == 0) {
-				;
-			}
-			else if(os_strcmp(item->valuestring, "set_hsv") == 0) {
-				;
-			}
-			else if(os_strcmp(item->valuestring, "start_cf") == 0) {
-				;
-			}
-			else if(os_strcmp(item->valuestring, "set_name") == 0) {
-				;
-			}
-			else {
-				os_sprintf(szRet, "error method[%s]", item->valuestring);
-			}
-#if 0			
-			iArraySize = cJSON_GetArraySize(itemArray);
-			for (index = 0; index < iArraySize; index++)
-			{
-				item = cJSON_GetArrayItem(itemArray, index);
-				if (NULL == item)  
-				{  
-					os_sprintf(szRet, "params is null");
-					break ;
-				}
-				if(item->type==cJSON_String)
-				{
-					if(os_strcmp(item->valuestring, "power") == 0)
-					{
-					}
-				}
-				else if(item->type == cJSON_Number)
-				{
-					os_memset(tmp, 0, sizeof(tmp));
-					os_sprintf(tmp, "%d", item->valueint);
-					os_strcat(szRet, tmp);
-				}
-				else
-				{
-					os_memset(szRet, 0, sizeof(szRet));
-					os_sprintf(szRet, "array value is not string or int");
-					break;
-				}
-			}
-#endif
-		}while(0);
-
-//		out=cJSON_Print(json);
-		cJSON_Delete(json);
-//		os_memcpy(szRet, out, os_strlen(out)<=sizeof(szRet)?os_strlen(out):sizeof(szRet));
-//		os_free(out);
-	}
-	Espconn_MutliSend(szRet);
-}
-
-void Espconn_MutliSendFn(void *arg)
-{
-	struct espconn *EspconnOfMutliSend = arg;
-}
-
-void Espconn_MutliReFn(void *arg, sint8 error)
-{
-	struct espconn *EspconnOfMutliRe = arg;
-	if (EspconnOfMutliRe != NULL)
-		Espconn_MutliRemove(EspconnOfMutliRe->reverse);
-}
-
-void Espconn_MutliListen(void *arg)
-{
-	struct espconn *EspconnOfMutliListen = arg;
-	uint8 ListenCount = 0;
-	
-	if (EspconnOfMutliListen){
-		for (ListenCount = 0; ListenCount < ESPCONN_MAXMUTLI; ListenCount ++){
-			if (TransportList[ListenCount].ListenFlag == FALSE){
-				TransportList[ListenCount].ListenFlag = TRUE;
-				TransportList[ListenCount].EspconnListen = EspconnOfMutliListen;
-				break;
-			}
-		}
-		
-		if (ListenCount >= ESPCONN_MAXMUTLI){
-			espconn_disconnect(EspconnOfMutliListen);
-			return;
-		}
-		EspconnOfMutliListen->reverse = &TransportList[ListenCount];
-		//regist success send callback;
-		espconn_regist_sentcb(EspconnOfMutliListen,Espconn_MutliSendFn);
-		//regist success Receive callback;
-		espconn_regist_recvcb(EspconnOfMutliListen,Espconn_MutliRecvFn);
-		//regist normal disconnect callback;
-		espconn_regist_disconcb(EspconnOfMutliListen,Espconn_MutliDisFn);
-		//regist abnormal disconnect callback;
-		espconn_regist_reconcb(EspconnOfMutliListen,Espconn_MutliReFn);
-	}else
-		return;
-}
-
-int8 Espconn_Mutli(uint32 port)
-{
-	struct espconn *MutliEspconn = (struct espconn *)os_zalloc(sizeof(struct espconn));
-	if(MutliEspconn != NULL){		
-		MutliEspconn->proto.tcp = (esp_tcp*)os_zalloc(sizeof(esp_tcp));
-		if (MutliEspconn->proto.tcp != NULL){
-			MutliEspconn->type = ESPCONN_TCP;
-			MutliEspconn->state = ESPCONN_NONE;
-			MutliEspconn->proto.tcp->local_port = port;
-			espconn_regist_connectcb(MutliEspconn,Espconn_MutliListen);
-			os_memset(TransportList, 0, sizeof(TransportList));
-			espconn_accept(MutliEspconn);
-		}else{
-			os_free(MutliEspconn);
-			return ESPCONN_MEM;
-		}
-	}else
-		return ESPCONN_MEM;
-}
-
-#if 0 
-static
-void gpio_intr_handler()
-{
-	uint32 gpio_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
-	GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status);
-	if(gpio_status & (BIT(0)))
-	{
-		if(GPIO_INPUT_GET(2)==0) {
-			GPIO_OUTPUT_SET(GPIO_ID_PIN(2),1);
-		}
-		else {
-			GPIO_OUTPUT_SET(GPIO_ID_PIN(2),0);
-		}
-	}
+/*
+ * set wifi station
+ */
+void user_set_station_config(){
+	struct station_config stConf;
+	stConf.bssid_set = 0;
+	os_memcpy(stConf.ssid, SSID, 32);
+	os_memcpy(stConf.password, PASSWORD, 64);
+	wifi_set_opmode(STATION_MODE);
+	wifi_station_set_config(&stConf);
 	return ;
 }
+
+/*
+ * wifi callbak 
+ */
+void wifi_handle_even_cb(System_Event_t *evt)
+{
+	switch(evt->event){
+		case EVENT_STAMODE_GOT_IP:
+			os_printf("Have got IP!!\n");
+			break;
+		defaut: 
+			os_printf("Have'n got IP\n");
+			break;
+	}
+}
+
+void ICACHE_FLASH_ATTR
+webserver_recv(void *arg, char *pusrdata, unsigned short length)
+{
+	;
+}
+
+void ICACHE_FLASH_ATTR
+webserver_recon(void *arg, sint8 err)
+{
+	struct espconn *pesp_conn = arg;
+
+	os_printf("webserver's %d.%d.%d.%d:%d err %d reconnect\n", pesp_conn->proto.tcp->remote_ip[0],
+			pesp_conn->proto.tcp->remote_ip[1],pesp_conn->proto.tcp->remote_ip[2],
+			pesp_conn->proto.tcp->remote_ip[3],pesp_conn->proto.tcp->remote_port, err);
+}
+
+void ICACHE_FLASH_ATTR
+webserver_discon(void *arg)
+{
+	struct espconn *pesp_conn = arg;
+
+	os_printf("webserver's %d.%d.%d.%d:%d disconnect\n", pesp_conn->proto.tcp->remote_ip[0],
+				pesp_conn->proto.tcp->remote_ip[1],pesp_conn->proto.tcp->remote_ip[2],
+				pesp_conn->proto.tcp->remote_ip[3],pesp_conn->proto.tcp->remote_port);
+}
+
+void ICACHE_FLASH_ATTR
+webserver_listen(void *arg)
+{
+	struct espconn *pesp_conn = arg;
+
+	espconn_regist_recvcb(pesp_conn, webserver_recv);
+	espconn_regist_reconcb(pesp_conn, webserver_recon);
+	espconn_regist_disconcb(pesp_conn, webserver_discon);
+}
+
+void ICACHE_FLASH_ATTR
+tcp_server(int port, espconn_connect_callback serListen)
+{
+	LOCAL struct espconn esp_conn_ser;
+	LOCAL esp_tcp esptcp;
+
+	esp_conn_ser.type = ESPCONN_TCP;
+	esp_conn_ser.state = ESPCONN_NONE;
+	esp_conn_ser.proto.tcp = &esptcp;
+	esp_conn_ser.proto.tcp->local_port = port;
+	espconn_regist_connectcb(&esp_conn_ser, serListen);
+
+	sint8 ret = espconn_accept(&esp_conn_ser);
+	os_printf("%d\n", ret);
+	if(!ret){
+		os_printf("Begin to listen!!!\n");
+	}
+	else{
+		os_printf("Fail to listen!!!\n");
+	}
+}
+
+
+#if 0
+void ICACHE_FLASH_ATTR
+user_udp_send(void)
+{
+	char hwaddr[6];
+	char DeviceBuffer[40]={0};
+	LOCAL char udp_remote_ip[1]={255,255,255,255};
+
+	user_udp_espconn.type=ESPCONN_UDP; 
+	user_udp_espconn.proto.udp= (esp_udp*)os_zalloc(sizeof(esp_udp)); 
+	os_memset(user_udp_espconn.proto.udp, 0, sizeof(esp_udp));
+	user_udp_espconn.proto.udp->local_port=2525; 
+	user_udp_espconn.proto.udp->remote_port=1982;
+	os_memcpy(user_udp_espconn.proto.udp->remote_ip,udp_remote_ip)
+	espconn_create(&user_udp_espconn);
+
+	wifi_get_macaddr(STATION_IF,hwaddr);
+	os_sprintf(DeviceBuffer,"设备MAC地址为"MACSTR"!!\r\n",MAC2STR(hwaddr));
+	espconn_sent(&user_udp_espconn,DeviceBuffer,os_strlen(DeviceBuffer));
+}
+
+void ICACHE_FLASH_ATTR
+user_udp_sent_cb(void *arg){
+	os_timer_disarm(&test_timer);
+	os_timer_setfn(&test_timer,user_udp_send,NULL);
+	os_timer_arm(&test_timer,1000,NULL);
+}
 #endif
-static 
-void gpio_intr_handler()
+
+void ICACHE_FLASH_ATTR
+gpio_intr_handler()
 {
 	uint32 gpio_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
 	GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, gpio_status);
@@ -270,18 +154,23 @@ void gpio_intr_handler()
 	}
 }
 
-
-void ICACHE_FLASH_ATTR user_init(void)
+void ICACHE_FLASH_ATTR 
+user_init(void)
 {
-	//wifi setting station mode
-	struct station_config stConf;
-	stConf.bssid_set = 0; 
-	os_memcpy(stConf.ssid, SSID, 32);
-	os_memcpy(stConf.password, PASSWORD, 64);
-	wifi_set_opmode(STATION_MODE);
-	wifi_station_set_config(&stConf);
-	Espconn_Mutli(9988);
-
+	//	set wifi station  mode
+	user_set_station_config();
+//	set wifi connect ap callback;
+//	wifi_set_event_handler_cb(wifi_handle_even_cb);
+	uint8 wifi_sat = wifi_station_get_connect_status();
+	if(wifi_sat!=STATION_GOT_IP){
+		os_printf("Wifi connect fail[%d]", wifi_sat);
+		return;
+	}
+	os_printf("Wifi connect success!");
+	
+	// start tcp server
+	tcp_server(9988, webserver_listen);
+	
 	os_delay_us(10000);
 
 	gpio_init();
@@ -295,5 +184,5 @@ void ICACHE_FLASH_ATTR user_init(void)
 	ETS_GPIO_INTR_ATTACH(&gpio_intr_handler, NULL);
 	gpio_pin_intr_state_set(GPIO_ID_PIN(0), GPIO_PIN_INTR_NEGEDGE);
 	ETS_GPIO_INTR_ENABLE();
-	return ;
+	return;
 }
